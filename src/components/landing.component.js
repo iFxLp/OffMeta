a629b3d8f3684e0180c173ca82bba5e37f4f1daf
Jesus <jesusojedapvcs@gmail.com>
2020-02-02 20:44:25 -0800

    Update landing.component.js
    get started button takes user to sign up

--- src/components/landing.component.js
+++ src/components/landing.component.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-
+import {withRouter} from 'react-router-dom'
 export default class  extends Component {
+    onSubmit = () => {
+        this.props.history.push('/registration');
+    }
     render() {
         return (
             <div className="landing">
                 <h1>Start a fantasy league today</h1>
                 <h6 style={{color: '#819091'}}>Create a free fantasy esports league in just 60 seconds</h6>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="get-started">
                     <input type="email" className="email-get-started" placeholder="Email"/>
                     <button type="submit" className="btn-get-started">Get Started</button>
