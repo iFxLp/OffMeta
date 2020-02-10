6d3059664d6388d6332c04f987dd1d4b6fcd5e04
Jesus <jesusojedapvcs@gmail.com>
2020-02-10 00:06:41 -0800

    Tab to homepage starter code

--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
 
-
 export default class HomePage extends Component{
+  
     render(){
         return(
             <div className ="homePage">
@@ -27,15 +27,41 @@ export default class HomePage extends Component{
                 </div>
                 
                 <div className="main-content">
-                    <div className="tabs">
-                        
+                
+                    <div class="tab">
+                    <button class="tablinks" onclick="openTab(event, 'Matchup')">Matchup</button>
+                    <button class="tablinks" onclick="openTab(event, 'My Team')">My Team</button>
+                    <button class="tablinks" onclick="openTab(event, 'League')">League</button>
+                    <button class="tablinks" onclick="openTab(event, 'Settings')">Settings</button>                    
                     </div>
+                    <div id="Matchup" class="tabcontent">
+                    <h3>Matchup</h3>
+                    <p>you are in Matchup</p>
+                    </div>
+
+                    <div id="MyTeam" class="tabcontent">
+                    <h3>MyTeam</h3>
+                    <p>you are in my team</p>
+                    </div>
+
+                    <div id="League" class="tabcontent">
+                    <h3>League</h3>
+                    <p>you are in league</p>
+                    </div> 
+
+                    <div id="Settings" class="tabcontent">
+                    <h3>Settings</h3>
+                    <p>you are in settings</p>
+                    </div> 
                 </div>
 
                 <div className="side-chat">
 
                 </div>
+                
             </div>
+
+            
         );
 
     }
